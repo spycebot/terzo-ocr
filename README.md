c19fad373c6f8caa34403148b32d6da2b19a0bd6
Shannon Ware <photius@shannonware.com>
2025-06-18 21:10:26 +0100

    Add name...main and PIL import.

---
+++ README.md
@@ -0,0 +1,12 @@
+# Terzo OCR
+
+18 JUN 2025
+
+## Summary
+
+### Dependencies
+
+Python 3.13
+Pillow-11.2.1
+argparse
+os
---
+++ app.py
@@ -0,0 +1,13 @@
+#! python3
+
+# import the necessary packages
+from PIL import Image
+# import pytesseract
+import argparse
+# import cv2
+import os
+
+
+
+if __name__ == "__main__":
+    print("Hello", "World", sep=", ", end="!")

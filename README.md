981d5f99739bf6c5975d9f32fdcda630ed367e63
Shannon Ware <photius@shannonware.com>
2025-06-21 17:15:45 +0100

    Factor in PIL and PyTesseract code from legacy script.

--- README.md
+++ README.md
@@ -10,3 +10,16 @@ Python 3.13
 Pillow-11.2.1
 argparse
 os
+
+### Installing PyTesseract
+
+1. Install tesseract using the Windows installer available at: https://github.com/UB-Mannheim/tesseract/wiki
+    a. Under "Choose Components", choose additional languages
+    b. For example, "Math / equation detection module"
+3. Take note of the tesseract path from the installation. For example: C:\Users\USER\AppData\Local\Tesseract-OCR. It may change so please check the installation path.
+3. Run "pip install pytesseract" in the virtual environment
+4. Set the tesseract path in the script before calling image_to_string:
+
+pytesseract.pytesseract.tesseract_cmd = r'C:\Users\USER\AppData\Local\Tesseract-OCR\tesseract.exe'
+
+(https://stackoverflow.com/questions/50951955/pytesseract-tesseractnotfound-error-tesseract-is-not-installed-or-its-not-i)
--- app.py
+++ app.py
@@ -2,12 +2,69 @@
 
 # import the necessary packages
 from PIL import Image
-# import pytesseract
+import pytesseract
 import argparse
-# import cv2
+import cv2
 import os
 
 
 
+def fetch_arguments():
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required=True,
+                    help="path to input image to be OCR'd")
+    ap.add_argument("-p", "--preprocess", type=str, default="thresh",
+                    help="type of preprocessing to be done: thresh or blur")
+    return vars(ap.parse_args())
+
+def load_image():
+    # Load the example image and convert it to grayscale
+    return cv2.imread(args["image"])
+
+def set_grayscale():
+    # Convert loaded image to grayscale
+    gray = cv2.cvtColor(target_image, cv2.COLOR_BGR2GRAY)
+    # Check to see if we should apply thresholding to preprocess the image
+    if args["preprocess"] == "thresh":
+        gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
+    elif args["preprocess"] == "blur":
+        gray = cv2.medianBlur(gray, 3)
+    return gray
+
+def write_grayscale():
+    # Write the grayscale image to disk as a temporary file so we can apply OCR to it
+    _filename = "{}.png".format(os.getpid())
+    cv2.imwrite(_filename, gray_scale)
+    return _filename
+
+def output_text(_filename):
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    text = pytesseract.image_to_string(Image.open(_filename))
+    '''try:
+    except:
+        print("An error occurred in pytesseract.image_to_string")
+    finally:
+        print("Finally for pytesseract.image_to_string")'''
+
+    try:
+        print(text)
+        os.remove(_filename)
+    except:
+        print("An error occurred in removing", str(_filename))
+    finally:
+        print("That is all she wrote.")
+
+def show_output_images():
+    # cv.imshow("Image", image)
+    pass
+
 if __name__ == "__main__":
-    print("Hello", "World", sep=", ", end="!")
+    print("Hello", "World", sep=", ", end="!\n")
+    args = fetch_arguments()
+    print("Image:", args["image"])
+    print("Preprocess:", args["preprocess"])
+    target_image = load_image()
+    gray_scale = set_grayscale()
+    filename = write_grayscale()
+    output_text(filename)
+    show_output_images()
